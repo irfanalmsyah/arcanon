95465f9036d36d666f28d0d51e313cb14b23bf14
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-04-06 13:32:38 +0700

    update user model

--- chat/models.py
+++ chat/models.py
@@ -3,10 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
-    gender = models.CharField(max_length=1, null=True, blank=True)
-    max_age = models.PositiveIntegerField(null=True, blank=True)
-    min_age = models.PositiveIntegerField(null=True, blank=True)
-    preference = models.CharField(max_length=1, null=True, blank=True)
+    gender = models.CharField(max_length=1, null=True, blank=True, choices=[('M', 'Male'), ('F', 'Female')])
+    age_pref = models.IntegerField(null=True, blank=True, choices=[
+            (0, 'Same'),
+            (1, 'Younger'),
+            (2, 'Older'),
+            (3, 'Younger or Same'),
+            (4, 'Older or Same'),
+            (5, 'Younger or Older'),
+        ])
+    gender_pref = models.CharField(max_length=1, null=True, blank=True, choices=[('M', 'Male'), ('F', 'Female')])
     
 class Room(models.Model):
     name = models.CharField(max_length=32, unique=True)
