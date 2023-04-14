4978298a96c1782b92da739ddb0df6e4596cb8e9
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-04-14 14:13:44 +0700

    change room url

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path("chat/<str:room_name>/", login_required(views.RoomView.as_view()), name="room"),
+    path("room/<str:room_name>/", login_required(views.RoomView.as_view()), name="room"),
     path("request/", login_required(views.RequesterView.as_view()), name="request"),
     path("respond/", login_required(views.ResponderView.as_view()), name="respond"),
 ]
