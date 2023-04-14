ecfa9b429321ab484954bb39606d0db1be7daee9
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-04-14 14:51:15 +0700

    add create post

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post, Comment
 
@@ -10,3 +10,16 @@ class PostView(View):
         comments = Comment.objects.filter(post=post).order_by('-date_posted')
         context = {'post': post, 'comments': comments}
         return render(request, 'post.html', context)
+    
+
+class CreatePostView(View):
+    def get(self, request):
+        return render(request, 'create_post.html')
+    def post(self, request):
+        # create a new post
+        post = Post.objects.create(
+            title=request.POST['title'],
+            content=request.POST['content'],
+            author=request.user
+        )
+        return redirect('post', post_id=post.id)
