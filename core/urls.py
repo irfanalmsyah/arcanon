1a8cc519ed1dc63d72a5179c109ba386aa8a4d9f
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-05-15 12:55:03 +0700

    add static

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
 
 urlpatterns = [
     path('', include('main.urls')),
     path('chat/', include('chat.urls')),
     path('forum/', include('forum.urls')),
-]
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
