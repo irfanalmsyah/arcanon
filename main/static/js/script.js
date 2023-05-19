b9a99e686941eaf5e62e372524b22659eacfc74f
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-05-20 00:21:56 +0700

    fix chrome issue

--- main/static/js/script.js
+++ main/static/js/script.js
@@ -1,6 +1,5 @@
 const sendAjaxRequest = (method, url, headers = {}, body = null) => {
     const csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
-    headers['content-type'] = 'aapplication/x-www-form-urlencoded';
     headers['X-CSRFToken'] = csrfToken;
         return fetch(url, { method, headers, body })
         .then(response => response.json())
@@ -11,9 +10,6 @@ const report = (type, id) => {
     const url = '/report/';
     const method = 'POST';
     const body = `type=${encodeURIComponent(type)}&id=${encodeURIComponent(id)}`;
-    const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    };
     sendAjaxRequest(method, url, headers, body);
   };
   
