721d4ce77b3a6739ace7dc4c2ddd42a765534c07
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-05-21 16:10:02 +0700

    fix url

--- core/urls.py
+++ core/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('forum/', include('forum.urls')),
     path('admin/', admin.site.urls),
 ]
-+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
