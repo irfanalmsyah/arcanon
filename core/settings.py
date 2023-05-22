83edd023140504cb1ec88f76efa9faa7ef0c0f10
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-05-22 14:09:23 +0700

    remove sqlite

--- core/settings.py
+++ core/settings.py
@@ -81,13 +81,6 @@ CHANNEL_LAYERS = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Jakarta'
